96ea13518d0e8da596ffad5745955c3b94be4714
Mausam Rayamajhi <mausamrayamajhi@Mausams-MBP.lan>
2025-11-20 08:04:13 -0600

    exposing flask as gloabl app for azure

--- app.py
+++ app.py
@@ -343,8 +343,8 @@ def create_app(config: dict | None = None):
 
     return app
 
+app = create_app()
 
 if __name__ == "__main__":
-    app = create_app()
     port = int(os.environ.get("PORT", "5000"))
     app.run(host="0.0.0.0", port=port, debug=True)
